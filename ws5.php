cdd6e89c99c6d8963ce66d5ebd9e3114d0a5d3d8
iamnatnai <92110407+iamnatnai@users.noreply.github.com>
2022-10-03 20:43:19 +0700

    Add files via upload

---
+++ ws5.php
@@ -0,0 +1,19 @@
+<?php include "connect.php" ?>
+<html>
+<head><meta charset="utf-8"></head>
+<body>
+<?php
+$stmt = $pdo->prepare(" SELECT * FROM member");
+$stmt->execute();?>
+
+<?php while ($row = $stmt->fetch()) : ?> 
+ชื่อสมาชิก :  <?= $row ["name"] ?>  <br> 
+ที่อยู่ :  <?=  $row ["address"] ?> <br>
+อีเมล :  <?= $row ["email"] ?>  <br>
+<a href="detail.php?username=<?=$row["username"]?>">
+<img src='../img/<?=$row["username"]?>.jpg' width='100'><br>
+</a>
+<hr><br>
+<?php endwhile; ?>
+</body>
+</html>
